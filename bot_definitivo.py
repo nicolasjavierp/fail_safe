85fcd4f8aeded687a059991c548533513f8574b8
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2018-11-22 14:25:25 -0300

    Fixing spam hello22

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -42,9 +42,9 @@ def increment_param_in_1_aux(item):
     with open(my_config_file, 'r') as f:
         aux = json.load(f)
         tmp = aux[item]
-        print("old value:" + tmp)
+        print("old value:" + str(tmp))
         aux[item] = tmp+1
-        print("new value:" + aux[item])
+        print("new value:" + str(aux[item]))
     with open("replayScript.json", "w") as f:
         json.dump(aux, f)
         
