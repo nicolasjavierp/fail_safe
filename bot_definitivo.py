e050e1b89d0b39c6266f0c9917ea465b906a29b2
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2018-12-06 14:40:21 -0300

    Rework Protocol and Ascendant 26

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -591,7 +591,7 @@ FEBRERO   26 ᴀʟᴛᴀʀ ᴀʙᴀɴᴅᴏɴᴀᴅᴏ - ᴊᴀʀᴅɪɴᴇs ᴅ
         4: ["IKELOS_SG_v1.0.1 (Escopeta)","https://cdn.discordapp.com/attachments/508999396835196950/520269571253600271/Escopeta.png"]
     }
 
-    embed = discord.Embed(title="" , description= context.message.author.mention+" esta semana toca: \n **"+ascendant_dict[key%7][0]+"**", color=0x00ff00)
+    embed = discord.Embed(title="" , description=":calendar: Esta semana el Desafío Ascendente es en: \n **"+ascendant_dict[key%7][0]+"**", color=0x00ff00)
     #embed = discord.Embed(title="Desafío Ascendente esta semana" , description=cont.format(ascendant_dict[key%7]), color=0x00ff00)
     #embed.set_thumbnail(url=client.user.avatar_url.replace("webp?size=1024","png"))
     #embed.set_author(name=client.user.name)#,icon_url=client.user.avatar_url.replace("webp?size=1024","png"))
