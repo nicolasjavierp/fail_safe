b4cf68f78412f239433e30f726cc122f7d19f8c8
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2018-12-06 10:50:03 -0300

    Rework Ascendant and Protocol calendar8

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -582,17 +582,18 @@ FEBRERO   26 ᴀʟᴛᴀʀ ᴀʙᴀɴᴅᴏɴᴀᴅᴏ - ᴊᴀʀᴅɪɴᴇs ᴅ
     }
     today = datetime.now()
     key = datetime.date(today).isocalendar()[1]
+
     cont= "```prolog\n\
 \n\
 {} \n\
 \n\
 ´´´"
-    #embed = discord.Embed(title="Desafío Ascendente esta semana" , description="KPO esta semana toca: "+ascendant_dict[key%7]+" ! :vulcan:", color=0x00ff00)
-    embed = discord.Embed(title="Desafío Ascendente esta semana" , description=cont.format(ascendant_dict[key%7]), color=0x00ff00)
-    embed.set_thumbnail(url=client.user.avatar_url.replace("webp?size=1024","png"))     
+    embed = discord.Embed(title="Desafío Ascendente esta semana" , description= context.message.author.mention+" esta semana toca: \n **"+ascendant_dict[key%7]+"** :vulcan:", color=0x00ff00)
+    #embed = discord.Embed(title="Desafío Ascendente esta semana" , description=cont.format(ascendant_dict[key%7]), color=0x00ff00)
+    embed.set_thumbnail(url=client.user.avatar_url.replace("webp?size=1024","png"))
     embed.set_author(name=client.user.name,icon_url=client.user.avatar_url.replace("webp?size=1024","png"))
     await client.send_message(context.message.channel, embed=embed)
-    await client.send_message(context.message.channel, "**"+ascendant_dict[key%7]+"**")
+    #await client.send_message(context.message.channel, "**"+ascendant_dict[key%7]+"**")
 
 
 @client.command(name='Run blacklist and populate clan',
