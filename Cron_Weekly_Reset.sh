0f9731fae2328cd74626c7dc0816b8144ba0df99
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-01-10 15:23:50 -0300

    Cron

--- Cron_Weekly_Reset.sh
+++ Cron_Weekly_Reset.sh
@@ -6,7 +6,9 @@
 # explode on errors
 set -e
 
-WEBHOOK="https://discordapp.com/api/webhooks/532255035141128192/OZYxXHCdlzSXtZAPgP8aBRtsHSuc8yQdXP4sbAZlqYN8BHVqmoY90BTz4ABnp9bkIVej"
+#WEBHOOK_E2_GENERAL="https://discordapp.com/api/webhooks/532255035141128192/OZYxXHCdlzSXtZAPgP8aBRtsHSuc8yQdXP4sbAZlqYN8BHVqmoY90BTz4ABnp9bkIVej"
+#WEBHOOK_TEST_INVITADOS="https://discordapp.com/api/webhooks/530403952752066580/yzWOFFnJidEpQycCrbE-st-5krSrAxAfrMAjOifl2Egsh9xSGK-y4NH-sWnUeWsyxNGM"
+WEBHOOK_E2_AVISOS="https://discordapp.com/api/webhooks/532986851037020161/e8zgy2iCIZMS_Xux3iL-6KF_5ylChdmJvI4jXq1pmHasG00BYrK2RhY05ANiZ6qAfITF"
 
 
 MESSAGE="\@everyone Estamos en horario del reset semanal !! Disfruten de Destiny2 !!"
@@ -15,4 +17,4 @@ USERNAME=DEFINITIVO
 curl -X POST \
      -F "content=${MESSAGE}" \
      -F "username=${USERNAME}" \
-     "${WEBHOOK}"
+     "${WEBHOOK_E2_AVISOS}"
