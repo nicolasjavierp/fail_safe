c2eec2beebe20bd99ad46a6b3df455cbfa2b82a4
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-02-12 15:48:47 -0300

    FIX cambio roles19

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -81,13 +81,13 @@ async def rol(context):
                 user_roles_names.append(i.name)
             #Get clan defined roles ids from discord
             for i in my_server.roles:
-                #if "Clan" in i.name:
-                    #custom_clan_role_id=i.id
+                if i.id == str(544911570258624522):
+                    custom_clan_role_id=i.id
+                if i.id == str(387742983249985536):
+                    custom_destiny_clan_role_id = i.id
                 if "DJ" in i.name:
                     custom_dj_role_id=i.id
 
-            custom_clan_role_id=544911570258624522
-            custom_destiny_clan_role_id=387742983249985536
             role_Clan = discord.utils.get(my_server.roles, id=custom_clan_role_id)
             role_DJ = discord.utils.get(my_server.roles, id=custom_dj_role_id)
             role_Destiny_Clan = discord.utils.get(my_server.roles, id=custom_destiny_clan_role_id)
@@ -547,7 +547,7 @@ async def testing(context):
             #print(user_has_role_destiny_clan)
             if user_has_role_destiny_clan:
                 print(str(memb.name)+" missing role ... adding ... ")
-                addroles = [role_Clan]
+                addroles = [role_DJ]
                 await client.add_roles(memb, *addroles)
             
 
