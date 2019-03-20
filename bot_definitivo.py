de91f583dfefb685a351733b6646bda38e000573
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-03-20 13:46:31 -0300

    res_test_3

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -216,9 +216,10 @@ async def raid_this_week(context):
             print("Valid User Destiny= "+str(user_destiny))
             user_destiny_id = user_destiny[0]['membershipId'] #From response extract the ID
             real_battletag = user_destiny[0]['displayName']
-            character_ids = user_destiny[1]
+            character_ids = user_destiny[0]
             print(user_destiny_id)
             print(real_battletag)
+            print(type(character_ids))
             print(character_ids)
 
 
