dc2c844f97b2d05ee176248bd23213d1c0f861ce
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-03-21 11:45:59 -0300

    res_test_35

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -252,6 +252,8 @@ async def raid_this_week(context):
                         offset = (today.weekday() - 1) % 7
                         last_tuesday = today - timedelta(days=offset)
                         print("Last_Tuesday:"+str(last_tuesday))
+                        print(type(last_tuesday))
+                        
                         last_tuesday_reset = datetime.combine(last_tuesday, datetime.time(17, 00))
                         #last_tuesday_reset = last_tuesday.replace(hour=17, minute=00)
                         print(last_tuesday_reset)
