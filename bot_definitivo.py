916d04891cc9b0374aa5f79d060dbf3c9f3b3db2
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-03-22 12:48:46 -0300

    res_test_60

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -232,7 +232,7 @@ async def raid_this_week(context):
                     completed = ""
                     for raid in raids_complete:
                         #print(raid['activityDetails']['instanceId']+" => "+str(fs.raids[raid['activityDetails']['directorActivityHash']]))
-                        completed = completed +"\n"+"Completo"+str(fs.raids[raid['activityDetails']['directorActivityHash']]) + " :white_check_mark: "
+                        completed = completed +"\n"+"Completo "+str(fs.raids[raid['activityDetails']['directorActivityHash']]) + " :white_check_mark: "
                     embed = discord.Embed(title=str(fs.guardian_class[info['classHash']])+" "+str(fs.guardian_race[info['raceHash']])+" este reset:", description=context.message.author.mention + completed, color=0x00ff00)
                     embed.set_thumbnail(url=client.user.avatar_url.replace("webp?size=1024","png")) 
                     await client.send_message(context.message.channel, embed=embed)
