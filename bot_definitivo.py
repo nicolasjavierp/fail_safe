ebc9ec1111fea73573c1394df5bf600c19b904ab
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-05-27 00:23:55 -0300

    brote_3

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -588,7 +588,7 @@ async def informe_semanal(context):
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/508999396835196950/520269693479551004/Protocolo.png")
     embed.set_image(url=protocol_dict[key%5][1])
     await client.send_message(context.message.channel, embed=embed)
-    embed = discord.Embed(title="" , description=":map: Mapa de Sala de Horno esta semana \n **"+HZ_dict[key%3][0]+"**", color=0x00ff00)
+    embed = discord.Embed(title="**Configuración "+HZ_dict[key%3][0]+"**" , description=":map: __Mapa de Sala de Horno__", color=0x00ff00)
     embed.set_image(url=HZ_dict[key%3][1])
     await client.send_message(context.message.channel, embed=embed)
 
