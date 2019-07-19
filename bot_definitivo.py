5c37cbcf0cc7ee6a4e93af1f84a9c9ef7558b3ba
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-07-19 15:40:50 -0300

    name_reset_20

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -800,7 +800,7 @@ async def reset_names(context):
                 print(memb.id)
                 print(memb.name)
                 print(memb.nick)
-                #await client.change_nickname(memb, memb.name)
+                await client.change_nickname(memb, memb.name)
             #if not memb.bot:
                 #print(memb.name)
                 #await client.change_nickname(memb, memb.name)
