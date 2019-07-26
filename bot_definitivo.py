ecdd8c2953e8fd718dd834f66b8df48d1836da00
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2019-07-26 14:48:36 -0300

    emblems pvp_19

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -1122,10 +1122,10 @@ async def testing(context):
                 for match in pvp_matches:
                     if "2017" in match['period']:
                         eligable=True
-                        break
                         print("Player played in 2017")
-                    else:
-                        print("Player DID NOT play in 2017")
+                        break
+                    #else:
+                    #    print("Player DID NOT play in 2017")
             if eligable:   
                 will_obtain_pi_emblem = False
                 for i in reversed(range(page_num-1)):
@@ -1138,21 +1138,10 @@ async def testing(context):
                         filtered_matches_list = filter_prismatic_inferno_emblem(pvp_matches)
                         if filtered_matches_list:
                             will_obtain_pi_emblem = True
+                            print(filtered_matches_list)
                             break
                         print("/***************************************/")
-                        #raids_complete_filtered = filter_completed_raids(raids_complete,fs)
-                        #print("/***************************************/")
-                        #print(raids_complete_filtered)
-                        #definitive_complete_raids=get_unique_raids(raids_complete_filtered, fs)
-                        #print(definitive_complete_raids)
-                        #for key, value in definitive_complete_raids.items():
-                        #    if value:
-                        #        report = report +" :white_check_mark: "+str(key) +"\n"
-                        #    else:
-                        #        report = report +" :x: "+ str(key) +"\n"
                     else:
-                        #for key, value in fs.relevant_raids.items():
-                        #    report = report + " :x: "+ value +"\n"
                         print("No PVP!!")
                         res = "ERROR!"
                 if will_obtain_pi_emblem:
