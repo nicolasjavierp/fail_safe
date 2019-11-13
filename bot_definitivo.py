2764cd42af4012055b7872d73b66d3fa2abc89d2
njp <nicolasjavierp@gmail.com.ar>
2019-11-13 14:09:02 -0300

    Fix Raids 3

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -469,14 +469,16 @@ async def raid_this_week(context):
                     raids = fs.get_CharactersRaids(user_destiny_id,character_id)
                     if raids:    
                         raids_complete = get_completed_raids(info,user_destiny_id,raids)
-                        #print("/***************************************/")
-                        #print(len(raids_complete))
-                        #print("/***************************************/")
+                        print("/***************************************/")
+                        print(len(raids_complete))
+                        print("/***************************************/")
                         raids_complete_filtered = filter_completed_raids(raids_complete,fs)
-                        #print("/***************************************/")
-                        #print(raids_complete_filtered)
+                        print("/***************************************/")
+                        print(raids_complete_filtered)
+                        print("/***************************************/")
                         definitive_complete_raids=get_unique_raids(raids_complete_filtered, fs)
-                        #print(definitive_complete_raids)
+                        print(definitive_complete_raids)
+                        print("/***************************************/")
                         for key, value in definitive_complete_raids.items():
                             if value:
                                 report = report +" :white_check_mark: "+str(key) +"\n"
