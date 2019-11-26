ab0f44e25c836c99d48f9c8b3f6951b51df1551b
njp <nicolasjavierp@gmail.com.ar>
2019-11-26 15:11:35 -0300

    fixing Xur 21

--- fail_safe.py
+++ fail_safe.py
@@ -615,7 +615,7 @@ class FailSafe(object):
             headers={"X-API-Key":self.api_key}
             async with aiohttp.get(site_call,headers=headers) as request:
                 if request.status == 200:
-                    return request.json()['Response']
+                    return (await request.json())
                 else:
                     return None
 
