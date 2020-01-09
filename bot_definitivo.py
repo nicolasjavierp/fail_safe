455d2ac451de28a79b9f56f915d9c630a39e10c9
njp <nicolasjavierp@gmail.com.ar>
2020-01-09 16:08:45 -0300

    adjusting new year 1

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -491,9 +491,9 @@ async def informe_lunar(ctx):
         3: ["Fallen Council",""]
     }
     altar_dict={
-        1: ["Escopeta","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/2f61559b7c57894703b6aaa52a44630c.jpg","https://www.light.gg/db/items/2782847179"],
-        2: ["Francotirador","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/b990412136d220fd641078418a4903fe.jpg","https://www.light.gg/db/items/2164448701"],
-        0: ["Lanzacohetes","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/eaf113dbb5cea03526009e6030b8c8ee.jpg","https://www.light.gg/db/items/3067821200"]
+        0: ["Escopeta","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/2f61559b7c57894703b6aaa52a44630c.jpg","https://www.light.gg/db/items/2782847179"],
+        1: ["Francotirador","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/b990412136d220fd641078418a4903fe.jpg","https://www.light.gg/db/items/2164448701"],
+        2: ["Lanzacohetes","https://cdn.thetrackernetwork.com/destiny/common/destiny2_content/icons/eaf113dbb5cea03526009e6030b8c8ee.jpg","https://www.light.gg/db/items/3067821200"]
     }
     today = datetime.now()
     key = datetime.date(today).isocalendar()[1]
@@ -513,7 +513,9 @@ async def informe_lunar(ctx):
         day_of_year = day_of_year-1
     else:
         pass
-
+    
+    print(key)
+    print(day_of_year)
     active_altar_weapon=altar_dict[day_of_year%3][0]
     altar_ordered_list=["Escopeta","Francotirador","Lanzacohetes"]
 
