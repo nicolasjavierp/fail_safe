6ebdb6e3344dc046ed7e42a3c0d68d94c7a42df1
njp <nicolasjavierp@gmail.com.ar>
2020-03-16 00:41:43 -0300

    testing role reset 11

--- bot_definitivo.py
+++ bot_definitivo.py
@@ -1593,7 +1593,7 @@ async def testing(ctx):
     #  'unban', 'vanity_invite', 'verification_level', 'voice_channels', 'voice_client', 'webhooks', 'widget']
 
     #canal_info=None
-    roles_to_remove = [{"id":587790059328110603, "name": },{"id":587790054177636362, "name": },{"id":587790049282752512, "name": },{"id":587790044971008040},{"id":587790042844364801},{"id":601115502378745868},{"id":601115840430997524},{"id":601115841635024929},{"id":601115984593420308}]
+    #roles_to_remove = [{"id":587790059328110603, "name": },{"id":587790054177636362, "name": },{"id":587790049282752512, "name": },{"id":587790044971008040},{"id":587790042844364801},{"id":601115502378745868},{"id":601115840430997524},{"id":601115841635024929},{"id":601115984593420308}]
     roles_list = []
 
     my_server = discord.utils.get(client.guilds)
@@ -1608,7 +1608,7 @@ async def testing(ctx):
     #for i in my_server.members:
     #    if  i.id == 376055309657047040:
     #        await i.remove_roles(roles_list)
-    
+
         #for rol in roles_to_remove:
         #    if  in i.name:
         #    custom_dj_role_id=i.id
